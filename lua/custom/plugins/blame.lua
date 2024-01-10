1f5ce67109683207d32bd298f5af1354b39c5248
Chris Moreton <chris.moreton@chaching.me>
2024-01-10 11:54:43 +0000

    Add Blame

--- lua/custom/plugins/blame.lua
+++ lua/custom/plugins/blame.lua
@@ -1,9 +1,11 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-vim.keymap.set('n', 'cp', '<Cmd>Copilot<CR>')
+vim.keymap.set('n', 'tb', '<Cmd>ToggleBlame<CR>')
+vim.keymap.set('n', 'eb', '<Cmd>EnableBlame<CR>')
+vim.keymap.set('n', 'db', '<Cmd>DisableBlame<CR>')
 
 return {
-  "github/copilot.vim",
+  "FabijanZulj/blame.nvim",
   version = "*",
   dependencies = {
   },
